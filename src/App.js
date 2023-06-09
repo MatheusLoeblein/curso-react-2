0b6e3761b1edf3cd24104e43a2ea0ed543c547c0
Matheus <matheus.eai@gmail.com>
2023-06-09 15:14:11 -0300

    Hooks: useMemo memorização de objetos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useMemo } from 'react';
 import './App.css';
 import P from 'prop-types';
 
-const Button = React.memo(function Button({ incrementButton }) {
+const Button = ({ incrementButton }) => {
   console.log('Filho Renderizou');
   return <button onClick={() => incrementButton(10)}>+</button>;
-});
+};
 Button.propTypes = {
   incrementButton: P.func,
 };
@@ -18,11 +18,15 @@ function App() {
 
   console.log('Pai Renderizou');
 
+  const btn = useMemo(() => {
+    return <Button incrementButton={incrementCounter} />;
+  }, [incrementCounter]);
+
   return (
     <div className="App">
       <p>Teste 1</p>
       <h1> C1: {counter} </h1>
-      <Button incrementButton={incrementCounter}></Button>
+      {btn}
     </div>
   );
 }
