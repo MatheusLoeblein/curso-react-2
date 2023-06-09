053833f2222d6a0ac247df90a07d12c7061c2efe
Matheus <matheus.eai@gmail.com>
2023-06-09 14:42:18 -0300

    Hooks: useEffect limpeza de lixo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
+const eventFn = () => {
+  console.log('H1 clickado');
+};
+
 function App() {
   const [counter, setCounter] = useState(0);
   const [counter2, setCounter2] = useState(0);
@@ -11,10 +15,16 @@ function App() {
   //   console.log('componentDidUpdate');
   // });
 
-  // // componentDidMount - executa uma vez
-  // useEffect(() => {
-  //   console.log('componentDidMount');
-  // }, []);
+  // componentDidMount - executa uma vez
+  useEffect(() => {
+    document.querySelector('h1')?.addEventListener('click', eventFn);
+
+    // ComponenteWillUmount - limpeza de lixos
+
+    return () => {
+      document.querySelector('h1')?.removeEventListener('click', eventFn);
+    };
+  }, []);
 
   // componentDidMount - com dependencia -  executa toda vez que dependencia mudar EX: [counter]
   useEffect(() => {
@@ -23,6 +33,7 @@ function App() {
 
   return (
     <div className="App">
+      <p>Teste 1</p>
       <h1>
         C1: {counter} C2: {counter2}
       </h1>
