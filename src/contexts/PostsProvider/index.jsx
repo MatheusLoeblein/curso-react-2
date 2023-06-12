536411fa4c1f6601c2d810131979e786557a422b
Matheus <matheus.eai@gmail.com>
2023-06-12 15:06:03 -0300

    Using everything together and organization

---
+++ src/contexts/PostsProvider/index.jsx
@@ -0,0 +1,19 @@
+import { useReducer } from 'react';
+import { PostsContext } from './context';
+import { data } from './data';
+import { reducer } from './reducer';
+import P from 'prop-types';
+
+export const PostsProvider = ({ children }) => {
+  const [postsState, postsDispatch] = useReducer(reducer, data);
+
+  return (
+    <PostsContext.Provider value={{ postsState, postsDispatch }}>
+      {children}
+    </PostsContext.Provider>
+  );
+};
+
+PostsProvider.propTypes = {
+  children: P.node.isRequired,
+};
---
+++ src/templates/App/index.jsx
@@ -0,0 +1,15 @@
+import './styles.css';
+import { PostsProvider } from '../../contexts/PostsProvider';
+import { Posts } from '../../components/Posts';
+// App.jsx
+function App() {
+  return (
+    <PostsProvider>
+      <div>
+        <Posts />
+      </div>
+    </PostsProvider>
+  );
+}
+
+export default App;
