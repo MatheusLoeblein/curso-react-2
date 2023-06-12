818684e5f05e2e4eb04ebe7e279306c124f36a0b
Matheus <matheus.eai@gmail.com>
2023-06-12 17:39:54 -0300

    My Hooks two

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,19 +1,19 @@
+import { useState } from 'react';
 import './styles.css';
 
-import { PostsProvider } from '../../contexts/PostsProvider';
-import { CounterProvider } from '../../contexts/ExampleProvider';
-import { Posts } from '../../components/Posts';
+/* eslint-disable */
+const UseFetch = (url, options) => {
+    const [result, setResult] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    result[result, loading]
+
+
+};
 
 function App() {
-  return (
-    <CounterProvider>
-      <PostsProvider>
-        <div>
-          <Posts />
-        </div>
-      </PostsProvider>
-    </CounterProvider>
-  );
+  const [result, loading] = UseFetch('https://jsonplaceholder.typicode.com/posts');
+  return <h1>Oi</h1>;
 }
 
 export default App;
