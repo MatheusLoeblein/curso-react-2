e52ac51049fda9efcc94e7c5aea64256f40aedbd
Matheus <matheus.eai@gmail.com>
2023-06-13 11:51:23 -0300

    Hooks: My second hook, useFetch

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,19 +1,43 @@
-import { useState } from 'react';
-import './styles.css';
+import { useState, useEffect } from 'react';
+import { useFetch } from './use-fetch';
 
-/* eslint-disable */
-const UseFetch = (url, options) => {
-    const [result, setResult] = useState(null);
-    const [loading, setLoading] = useState(false);
+export const Home = () => {
+  const [postId, setPostId] = useState('');
+  const [result, loading] = useFetch(
+    'https://jsonplaceholder.typicode.com/posts/' + postId,
+    {
+      headers: { ABC: 1 },
+    },
+  );
 
-    result[result, loading]
+  useEffect(() => {
+    console.log('ID Post', postId);
+  }, [postId]);
 
+  if (loading) {
+    return <p>loading...</p>;
+  }
 
-};
+  const handleClick = (id) => {
+    setPostId(id);
+  };
+  if (!loading && result) {
+    return (
+      <div>
+        {result?.length > 0 ? (
+          result.map((p) => (
+            <div key={`post-${p.id}`} onClick={() => handleClick(p.id)}>
+              <p>{p.title}</p>
+            </div>
+          ))
+        ) : (
+          <div onClick={() => handleClick('')}>
+            <p>{result.title}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
 
-function App() {
-  const [result, loading] = UseFetch('https://jsonplaceholder.typicode.com/posts');
   return <h1>Oi</h1>;
-}
-
-export default App;
+};
