d7f7fe5474359aa9159aef27cc7d3d4211c6b265
Matheus <matheus.eai@gmail.com>
2023-06-14 15:00:38 -0300

    Error Boundaries - Controlando erros

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,48 +1,72 @@
-import { useDebugValue, useEffect, useState } from 'react';
+import { useEffect, useState, Component } from 'react';
+import PropTypes from 'prop-types';
 
-const useMediaQuery = (queryValue, initialValue = false) => {
-  const [match, setMatch] = useState(initialValue);
+class MyErroBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
 
-  useDebugValue(`Query ${queryValue}`, (name) => {
-    return name + ' Modificado';
-  });
+  static getDerivedStateFromError(error) {
+    // Atualiza o state para que a próxima renderização mostre a UI alternativa.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // Você também pode registrar o erro em um serviço de relatórios de erro
+    // console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Você pode renderizar qualquer UI alternativa
+      return <h1>Algo deu errado.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+MyErroBoundary.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const ItWillThrowError = () => {
+  const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-    let isMounted = true;
-    const matchMedia = window.matchMedia(queryValue);
-    const handleChange = () => {
-      if (!isMounted) return;
-      setMatch(Boolean(matchMedia.matches));
-    };
+    if (counter > 3) {
+      throw new Error('Que Chato');
+    }
+  }, [counter]);
 
-    matchMedia.addEventListener('change', handleChange);
-
-    setMatch(!!matchMedia.matches);
-
-    return () => {
-      isMounted = false;
-      matchMedia.removeEventListener('change', handleChange);
-    };
-  }, [queryValue]);
-
-  return match;
+  return (
+    <div>
+      <button
+        style={{ fontSize: '50px' }}
+        onClick={() => setCounter((s) => s + 1)}
+      >
+        Click to increase {counter}
+      </button>
+    </div>
+  );
 };
 
 export const Home = () => {
-  const huge = useMediaQuery('(min-width: 980px)');
-  const big = useMediaQuery('(max-width: 979px) and (min-width: 768px)');
-  const medium = useMediaQuery('(max-width: 767px) and (min-width: 521px)');
-  const small = useMediaQuery('(max-width: 521px)');
-
-  const background = huge
-    ? 'green'
-    : big
-    ? 'red'
-    : medium
-    ? 'yellow'
-    : small
-    ? 'purple'
-    : null;
-
-  return <div style={{ fontSize: '50px', background }}>Oi</div>;
+  return (
+    <div>
+      <MyErroBoundary>
+        <ItWillThrowError />
+      </MyErroBoundary>
+      <MyErroBoundary>
+        <ItWillThrowError />
+      </MyErroBoundary>
+      <MyErroBoundary>
+        <ItWillThrowError />
+      </MyErroBoundary>
+      <MyErroBoundary>
+        <ItWillThrowError />
+      </MyErroBoundary>
+    </div>
+  );
 };
